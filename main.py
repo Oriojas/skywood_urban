755899fdf1a21f3c7a593b94f3941bd20f83f784
Oriojas <oscarriojas@gmail.com>
2022-11-11 01:12:48 -0500

    feat: create endpoint send_data

---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+import pyodbc
+import uvicorn
+import time
+import pandas as pd
+from datetime import datetime
+from post import postIpfs
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+SERVER = os.environ["SERVER"]
+INSTANCE = os.environ["INSTANCE"]
+DATABASE = os.environ["DATABASE"]
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSWORD"]
+DRIVER = os.environ["DRIVER"]
+TOKEN = os.environ["TOKEN"]
+ROWS = os.environ["ROWS"]
+
+with open('temp_data/init.txt', 'w') as f:
+    f.write(str(0))
+
+df = pd.DataFrame(columns=['CO2', 'DATE_C', 'ORIGIN'])
+df.to_csv('temp_data/temp_data.csv')
+
+
+@app.get('/send_data/')
+async def send_data(co2: int, origin: str, token: str):
+    """
+    this endpoint send data ti ipfs and index in SQL database
+    :param co2: co2 in ppm from sensor
+    :param origin: name sensor
+    :param token: token from sensor
+    :return:
+    """
+
+    if token == TOKEN:
+
+        df_co = pd.read_csv('temp_data/temp_data.csv', index_col=0)
+
+        date_c = datetime.today().strftime('%Y-%m-%d_%H-%M')
+
+        df_co = df_co.append({'CO2': co2,
+                              'DATE_C': date_c,
+                              'ORIGIN': origin}, ignore_index=True)
+
+        if len(df_co) > int(ROWS):
+            df_co = df_co.drop([0], axis=0)
+        df_co.to_csv('temp_data/temp_data.csv')
+
+        with open('temp_data/init.txt') as file:
+            cont = file.read()
+
+        cont = int(cont)
+        cont += 1
+        with open('temp_data/init.txt', 'w') as file:
+            file.write(str(cont))
+        print(f'counter : {cont}')
+
+        if cont == int(ROWS):
+            file_name = df_co['DATE_C'].iloc[-1]
+            print(file_name)
+            temp_file = f"temp_data/{file_name}.json"
+            df_co.to_json(temp_file)
+            time.sleep(30)
+            cid, ret_url = postIpfs(file_name=str(file_name)).send_data()
+            print(f'saveIpfs : {file_name}, {ret_url}')
+
+            with pyodbc.connect(
+                    'DRIVER=' + DRIVER + ';SERVER=tcp:' + SERVER + ';PORT=1433;DATABASE=' + DATABASE + ';UID=' + USERNAME + ';PWD=' + PASSWORD) as conn:
+                with conn.cursor() as cursor:
+                    count = cursor.execute(
+                        f"INSERT INTO {INSTANCE} (cid, ret_url, date) VALUES ('{cid}', '{ret_url}', '{file_name}');").rowcount
+                    conn.commit()
+                    print(f'Rows inserted: {str(count)}')
+
+            with open('temp_data/init.txt', 'w') as file:
+                file.write(str(0))
+
+            os.remove(temp_file)
+
+    else:
+        print('Bad token')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8088)
--- post.py
+++ post.py
@@ -1,32 +1,38 @@
 import os
+import json
 import subprocess
 
 KEY = os.environ["KEY"]
 
-url = "https://api.estuary.tech/content/add"
-aut = f'"Authorization: Bearer {KEY}"'
-con = '"Content-Type: multipart/form-data"'
-dat = '"data=@/home/oscar/Escritorio/test3.json"'
 
-post = f'curl -X POST {url} -H {aut} -H {con} -F {dat}'
+class postIpfs:
 
-resp = subprocess.run(post,
-                      stderr=subprocess.PIPE,
-                      stdout=subprocess.PIPE,
-                      text=True,
-                      shell=True)
+    def __init__(self, file_name):
 
-print(resp.stdout)
+        self.file_name = file_name
+        url = "https://api.estuary.tech/content/add"
+        aut = f'"Authorization: Bearer {KEY}"'
+        con = '"Content-Type: multipart/form-data"'
+        dat = f'"data=@/home/oscar/GitHub/skywood_urban/temp_data/{self.file_name}.json"'
 
-str({"cid":"bafkreialmz7zxqugptwwtaz54jbalyy5phvheirhpmqposklb74i6ilsja",
- "retrieval_url":"https://dweb.link/ipfs/bafkreialmz7zxqugptwwtaz54jbalyy5phvheirhpmqposklb74i6ilsja",
- "estuaryId":49229648,
- "providers":["/ip4/145.40.93.107/tcp/6745/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/127.0.0.1/tcp/6745/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/145.40.93.107/udp/6746/quic/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/127.0.0.1/udp/6746/quic/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/145.40.93.107/tcp/6747/ws/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/127.0.0.1/tcp/6747/ws/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/100.122.184.192/tcp/6745/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/100.104.120.192/tcp/6745/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7",
-              "/ip4/100.97.164.64/tcp/6745/p2p/12D3KooWBUriTeu6YoJsuSg5gCvEecim9xKdZaW8fE54LUzmWJn7"]})
+        self.post = f'curl -X POST {url} -H {aut} -H {con} -F {dat}'
+
+    def send_data(self):
+        resp = subprocess.run(self.post,
+                              stderr=subprocess.PIPE,
+                              stdout=subprocess.PIPE,
+                              text=True,
+                              shell=True)
+
+        try:
+            dict_resp = json.loads(resp.stdout)
+
+            cid = dict_resp.get('cid')
+            ret_url = dict_resp.get('retrieval_url')
+
+        except:
+
+            cid = "Bad Request"
+            ret_url = "Bad Request"
+
+        return cid, ret_url
