667870b8e76d2acff5e04ac657aebac3cfdb6dce
EC2 Default User <ec2-user@ip-172-31-86-52.ec2.internal>
2022-11-12 15:19:29 +0000

    feat: update .gitignore

--- script.sh
+++ script.sh
@@ -9,3 +9,4 @@ export TOKEN="0ce956fc-131b-42d6-a4b1-8e8319e45f84"
 export ROWS=20
 export KEY="EST0bb63752-eecc-4a90-8ae8-b034c2e8dc48ARY"
 export INSTANCE="polkadothack.dbo.index_co2_sensor"
+export FOLDER_DATA="/home/ec2-user/skywood_urban/temp_data/"
