cc0854b1694895d2b2c2b44ff792c9a31b0e25bc
EC2 Default User <ec2-user@ip-172-31-86-52.ec2.internal>
2022-11-12 14:56:01 +0000

    fix: envronment variables

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DATABASE=master
+export DRIVER="{ODBC Driver 17 for SQL Server}"
+export PASSWORD=Polkadot
+export PYTHONUNBUFFERED=1
+export SERVER="polkadothack.cnwuu2sdaqvk.us-east-1.rds.amazonaws.com"
+export USERNAME=admin
+export TOKEN="0ce956fc-131b-42d6-a4b1-8e8319e45f84"
+export ROWS=20
+export KEY="EST0bb63752-eecc-4a90-8ae8-b034c2e8dc48ARY"
+export INSTANCE="polkadothack.dbo.index_co2_sensor"
